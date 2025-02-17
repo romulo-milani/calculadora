9f12f4a5bce4e8ac4956d077d37736c61dd026bb
romulo.milani <81986766+romulo-milani@users.noreply.github.com>
2025-02-17 06:06:23 -0300

    criada a função principal chamada operar

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+/*Variáveis*/
+let operando1 = 0;
+let operando2 = 0;
+let operador = "";
+
+
+/*Funções da Calculadora*/
 function somar(a, b) {
     return a + b;
 }
@@ -12,4 +19,24 @@ function multiplicar(a, b) {
 
 function dividir(a, b) {
     return a / b;
+}
+
+function operar(operandoA, operandoB, operador) {
+    switch (operador) {
+        case "+":
+            return somar(operandoA, operandoB);
+            break;
+        
+        case "-":
+            return subtrair(operandoA, operandoB);
+            break;
+
+        case "*":
+            return multiplicar(operandoA, operandoB);
+            break;
+
+        case "/":
+            return dividir(operandoA, operandoB);
+            break;
+    }
 }
