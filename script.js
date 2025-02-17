6a62b2309c3bb25c4f0edb0457d5b83b8c9b9fd2
romulo.milani <81986766+romulo-milani@users.noreply.github.com>
2025-02-17 06:44:45 -0300

    preencher a tela quando ousuario digita um operador

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let operando2 = 0;
 let operador = "";
 let tecladoNumerico = document.querySelectorAll(".tecladoNumerico");
 let downTela = document.querySelector(".downtela");
+let op = document.querySelectorAll(".op");
 
 
 /*Funções da Calculadora*/
@@ -44,12 +45,23 @@ function operar(operandoA, operandoB, operador) {
 }
 
 function preencherTela(e) {
-    downTela.textContent += e.target.textContent;
+    downTela.textContent += e.target.textContent; //preenche a tela de baixo
+    operando1 += e.target.textContent; //armazena o primeiro valor na variavel operando1
 }
+
+function preencherTelaOperando(e) {
+    operador = e.target.textContent; // armazena o operador digitado na variavel operador
+    downTela.textContent += ` ${operador}`; //preenche a tela de baixo com o operador
+};
 
 /*Preencher a tela quando o usuário digita no teclado numérico*/
 for (let i = 0; i < tecladoNumerico.length; i++) {
-    tecladoNumerico[i].addEventListener('click', preencherTela);
+    tecladoNumerico[i].addEventListener('click', preencherTela);    
+}
+
+/*Preencher a tela quando o usuario pressiona o botão de operador, e armazenar o valor na variavel operador */
+for (let i = 0; i < op.length; i++) {
+    op[i].addEventListener("click", preencherTelaOperando);
 }
 
 
